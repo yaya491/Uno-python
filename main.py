548f4cb6525a9be571f8b60f90ec7f35e4023c0b
yaya491 <yannisamarir1@gmail.com>
2026-04-02 16:45:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 print("hello world")
 print("yannis est gay")
+
+def creer_paquet():
+    couleurs = ["rouge", "bleu", "vert", "jaune"]
+    valeurs = ["0","1","2","3","4","5","6","7","8","9"]
+
+    paquet = []
+
+    for couleur in couleurs:
+        for valeur in valeurs:
+            paquet.append((couleur, valeur))
+
+    return paquet
