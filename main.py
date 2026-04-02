0138b7a86ff03420c2bc16b8b5b76c2a0a39407b
LeChinois34 <andre.ollivier34@hotmail.com>
2026-04-02 16:54:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-print("hello world")
-print("yannis est gay")
-
 def creer_paquet():
     couleurs = ["rouge", "bleu", "vert", "jaune"]
     valeurs = ["0","1","2","3","4","5","6","7","8","9"]
@@ -12,3 +9,26 @@ def creer_paquet():
             paquet.append((couleur, valeur))
 
     return paquet
+
+
+import random
+
+couleurs = ["rouge", "bleu", "vert", "jaune"]
+
+def distribution_cartes():
+    for i in range(7): #Distribution des 7 catres par joueur
+        nombre = random.randint(0, 9)
+        couleur = random.choice(couleurs)
+        print(nombre, couleur)
+        
+while True:
+    joueurs = int(input("Combien de joueurs ?"))
+    if 2 <= joueurs <= 10:
+        break #donne la possibilité de quitter une boucle
+    print("Nombre de joueur invalide : choisit un autre nombre qui est compris entre 2 et 10 .")
+
+for j in range(joueurs):
+    print("Joueur", j + 1)
+    distribution_cartes()
+    
+    print()
