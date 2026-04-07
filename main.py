e59d4a44ad7a8d0ab7f99b16d460203041a078a0
LeChinois34 <andre.ollivier34@hotmail.com>
2026-04-07 11:45:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
+import random
+
 def creer_paquet():
     couleurs = ["rouge", "bleu", "vert", "jaune"]
     valeurs = ["0","1","2","3","4","5","6","7","8","9"]
-
     paquet = []
-
     for couleur in couleurs:
         for valeur in valeurs:
             paquet.append((couleur, valeur))
-
     return paquet
 
+def distribution_cartes(paquet, nb_cartes=7):
+    main = []
+    for _ in range(nb_cartes):
+        carte = paquet.pop()
+        main.append(carte)
+    return main
 
-import random
+def nombre_joueurs():
+    while True:  # Boucle jusqu'à une entrée valide
+        joueurs = int(input("Combien de joueurs ? "))
+        if 2 <= joueurs <= 10:
+            return joueurs
+        print("Nombre invalide : choisir entre 2 et 10.")
 
-couleurs = ["rouge", "bleu", "vert", "jaune"]
+# Programme principal
+paquet = creer_paquet()
+random.shuffle(paquet)
 
-def distribution_cartes():
-    for i in range(7): #Distribution des 7 catres par joueur
-        nombre = random.randint(0, 9)
-        couleur = random.choice(couleurs)
-        print(nombre, couleur)
-        
-while True:
-    joueurs = int(input("Combien de joueurs ?"))
-    if 2 <= joueurs <= 10:
-        break #donne la possibilité de quitter une boucle
-    print("Nombre de joueur invalide : choisit un autre nombre qui est compris entre 2 et 10 .")
+joueurs = nombre_joueurs()
 
 for j in range(joueurs):
-    print("Joueur", j + 1)
-    distribution_cartes()
-    
+    print(f"Joueur {j + 1} :")
+    main = distribution_cartes(paquet)
+    for carte in main:
+        print(" ", carte[1], carte[0])
     print()
